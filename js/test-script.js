13f4902e7c8dc8318f708d33d897a5966a3ee446
KutsevEA <kucev.ea@gmail.com>
2022-06-17 17:45:01 +0300

    тест из тестов

---
+++ js/test-script.js
@@ -0,0 +1,13 @@
+function getCommonElements(array1, array2) {
+  // Change code below this line
+    
+    const arrayTotal = [];
+
+    for (let i = 0; i < array1.length; i += 1) {
+        if ()
+    }
+    
+  // Change code above this line
+}
+
+getCommonElements([1, 2, 3], [2, 1, 17, 19]);
