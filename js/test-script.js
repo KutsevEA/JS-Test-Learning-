88ed89bdc1933f6c86580c00a54aca6153c0194f
KutsevEA <kucev.ea@gmail.com>
2022-06-21 18:58:45 +0300

    Update test-script.js

--- js/test-script.js
+++ js/test-script.js
@@ -1,17 +1,15 @@
-function includes(array, value) {
-  // Change code below this line
-    let x = false;
-  for (let i = 0; i < array.length; i += 1) {
-      if (array[i] === value) {
-          console.log(array[i]);
-          x = (array[i] === value);
-          return x
-    }
-    
-  }
-    return x;
-  // Change code above this line
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (const key in apartment) {
+keys.push(key);
+values.push(apartment[key]);
 }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes(['apple', 'plum', 'pear', 'orange'], 'kiwi'));
+
+console.log(values);
